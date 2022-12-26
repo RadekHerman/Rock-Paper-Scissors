e4ddf2060f8d9b0a0094fad2ac673d5c9a68f80b
RadekHerman <107422011+RadekHerman@users.noreply.github.com>
2022-12-26 23:45:19 +0100

    feat: compChoice

--- js/rps.js
+++ js/rps.js
@@ -1,1 +1,10 @@
-alert("ello form javascript")
+// alert("ello form javascript")
+
+const selection = ["Rock", "Paper", "Scissors"];
+
+function getComputerChoice() {
+    return selection[Math.floor(Math.random() * selection.length)];
+}
+
+computerSelection = getComputerChoice();
+console.log(computerSelection);
