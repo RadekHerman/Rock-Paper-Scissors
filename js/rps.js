5e1471cfabb32c36f234db92e1a081fa0fbf1603
RadekHerman <107422011+RadekHerman@users.noreply.github.com>
2023-02-09 18:39:25 +0100

    feat: js file is rps.js

--- js/rps.js
+++ js/rps.js
@@ -8,41 +8,24 @@ const finalResultPrint = document.getElementById('final-result');
 const roundResultPrint = document.getElementById('round-result');
 const compImage = document.getElementById('comp-image');
 
-//const imageBox = document.getElementsByClassName("image-box");
-// imageBox.addEventListener("mouseleave", () => {compImage.src="./img/blank.jpg"});
-//image.onmouseleave = () => {compImage.src="./img/blank.jpg"};
-// imageBox.addEventListener("mouseleave", leave);
-
-
-//image.addEventListener('mouseleave', leave);
-
-
-function leave() {
-    compImage.src="./img/blank.jpg";
-}
-
-
 function game() {
     compWinCount = 0;
     playerWinCount = 0;
-    //imageBox.addEventListener("mouseenter", () => {compImage.src="./img/blank.jpg"});
-    for (let i=0; i < image.length; i++ ) {
-        image[i].addEventListener('click', playerSelection)
-    }
+   
+	for (let i=0; i < image.length; i++ ) {
+	  image[i].addEventListener('click', playerSelection)
+	  image[i].addEventListener('mouseout', () => {
+		setTimeout(() => {
+		  compImage.src = "./img/blank.jpg";
+		}, 500);
+	  });
+	}
     
     roundResultPrint.textContent = ".................."
     finalResultPrint.textContent = ".................."
 	btn.onclick = null;
 	btn.style.cssText = 'color: lightgrey'
-	//btn.style.cssText = 'all: revert';
-    // compImage.src = `./img/blank.jpg`
-    // image.onmouseover = () => compImage.src=`./img/blank.jpg`
-
-    //image.o   nmouseover = () => {compImage.src="./img/blank.jpg"}
-    //image.addEventListener("mouseenter", () => {compImage.src="./img/blank.jpg"})
-
-    //imageBox.addEventListener("mouseenter", () => {compImage.src="./img/blank.jpg"});
-    //imageBox.addEventListener("mouseleave", () => {compImage.src="./img/blank.jpg"});
+	
     
 
 }
@@ -56,10 +39,6 @@ function computerSelection() {
 }
 
 function playRounds(playerChoice) {
-    // const selection = ["ROCK", "PAPER", "SCISSORS"];
-    // computerChoice = selection[Math.floor(Math.random() * selection.length)];
-    // console.log(computerChoice, 'computer')
-
     
     computerChoice = computerSelection();
     roundResult = winner(playerChoice, computerChoice);
@@ -69,6 +48,9 @@ function playRounds(playerChoice) {
     if (compWinCount >= 5 || playerWinCount >= 5) {
         for (let i=0; i < image.length; i++ ) {
             image[i].removeEventListener('click', playerSelection)
+			image[i].removeEventListener('mouseout', function() {
+				compImage.src = "./img/blank.jpg"
+			})
         } if (compWinCount >= 5 ) {
         finalResultPrint.textContent = `You LOST the match! ${playerWinCount} to ${compWinCount}`
 		} else {
@@ -78,6 +60,7 @@ function playRounds(playerChoice) {
 		btn.onclick = () => game() // btn pokarze się dopiero po skończonej grze 5 rund
 	}
 };
+
 
 function playerSelection(e) {
     playerChoice = e.target.id.toUpperCase();
